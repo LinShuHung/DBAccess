a3ec8c9f9652031f90ddd1d256cc58583c21f22a
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-08-21 11:15:51 +0800

    Fix bug: id redefine

--- app/src/main/java/com/suhun/dbaccess/MySQLiteOpenHelper.java
+++ app/src/main/java/com/suhun/dbaccess/MySQLiteOpenHelper.java
@@ -8,7 +8,7 @@ import androidx.annotation.Nullable;
 
 public class MySQLiteOpenHelper extends SQLiteOpenHelper {
     private final String createCustTable = "CREATE TABLE cust (cid INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            "cname TEXT, cid TEXT, cbirthday DATE)";
+            "cname TEXT, ctel TEXT, cbirthday DATE)";
     public MySQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
