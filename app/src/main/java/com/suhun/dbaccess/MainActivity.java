3ddbd17846679dee567767ff8b7a156b694edd56
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-08-21 10:53:13 +0800

    Initial TextView, EditText

--- app/src/main/java/com/suhun/dbaccess/MainActivity.java
+++ app/src/main/java/com/suhun/dbaccess/MainActivity.java
@@ -4,13 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private String tag = MainActivity.class.getSimpleName();
+    private TextView queryResult, birthday;
+    private EditText idUpdate, name, tel, idDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView(){
+        queryResult = findViewById(R.id.lid_queryResult);
+        birthday = findViewById(R.id.lid_birthdayInput);
+        idUpdate = findViewById(R.id.lid_whichIdUpdate);
+        name = findViewById(R.id.lid_nameInput);
+        tel = findViewById(R.id.lid_telInput);
+        idDelete = findViewById(R.id.lid_whichIdDelete);
     }
 
     public void birthdaySelectFun(View view){
