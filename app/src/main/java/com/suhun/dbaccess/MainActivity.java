1e60fa9328e37668a58c66de8d947d2da9d20020
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-08-21 11:39:54 +0800

    Add insert function

--- app/src/main/java/com/suhun/dbaccess/MainActivity.java
+++ app/src/main/java/com/suhun/dbaccess/MainActivity.java
@@ -2,9 +2,13 @@ package com.suhun.dbaccess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -33,20 +37,51 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initSQLiteDataBase(){
-        mySQLiteOpenHelper = new MySQLiteOpenHelper(this, "SuhunDB", null, 1);
+        mySQLiteOpenHelper = new MySQLiteOpenHelper(this, "suhunDB", null, 1);
         db = mySQLiteOpenHelper.getWritableDatabase();
     }
 
     public void birthdaySelectFun(View view){
+        DatePickerDialog dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                String result = String.format("%s-%s-%s", year, month+1, dayOfMonth);
+                birthday.setText(result);
+            }
+        }, 1977, 00, 01);
+        dialog.show();
 
     }
 
     public void queryFun(View view){
+        execQuery();
+    }
 
+    private void execQuery(){
+        StringBuffer stringBuffer = new StringBuffer();
+        Cursor cursor = db.query("cust", null, null, null, null, null, null);
+
+        while(cursor.moveToNext()){
+            String cid = cursor.getString(cursor.getColumnIndexOrThrow("cid"));
+            String cname = cursor.getString(cursor.getColumnIndexOrThrow("cname"));
+            String ctel = cursor.getString(cursor.getColumnIndexOrThrow("ctel"));
+            String cbirthday = cursor.getString(cursor.getColumnIndexOrThrow("cbirthday"));
+            String result = String.format("%s:%s:%s:%s\n", cid, cname, ctel, cbirthday);
+            stringBuffer.append(result);
+            queryResult.setText(stringBuffer);
+        }
     }
 
     public void insertFun(View view){
-
+        ContentValues values = new ContentValues();
+        values.put("cname", name.getText().toString());
+        values.put("ctel", tel.getText().toString());
+        values.put("cbirthday", birthday.getText().toString());
+        db.insert("cust", null, values);
+        execQuery();
+        name.setText("");
+        tel.setText("");
+        birthday.setText("");
     }
 
     public void updateFun(View view){
